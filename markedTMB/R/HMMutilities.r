7b9fc4e1349855140cde01b1dcdcb18ab91a9caf
Jeff Laake <jefflaake@gmail.com>
2021-12-01 13:54:59 -0800

    abundance estimate and bootstrap for MSJS type models

--- markedTMB/R/HMMutilities.r
+++ markedTMB/R/HMMutilities.r
@@ -53,7 +53,7 @@ compute_matrices=function(object,state.names=NULL,par=NULL)
   }
   if(object$model%in%c("MSJS","MSJSU"))
   {
-    names(mat)=c("p0","N","dmat","gamma")
+    names(mat)=c("N","p0","dmat","gamma")
     mat$delta=matrix(0,nrow=nrow(object$data$data),ncol=length(object$data$strata.labels)+1)
     mat$delta[,1]=1
     object$data$start=rbind(c(1,1),object$data$start)
@@ -195,42 +195,112 @@ global_decode=function(object,state.names=NULL,par=NULL)
 		psi=matrix(NA,nrow=T,ncol=m)
 		# Assign psi value at first occasion
 		psi[first[i],]=delta[i,]%*%diag(dmat[i,first[i],x[i,first[i]],]) 
+		psi[first[i],]=psi[first[i],]/sum(psi[first[i],])
 		for(t in (first[i]+1):T)
 		{
 			psi[t,]=apply(psi[t-1,]*gamma[i,t-1,,],2,max)%*%diag(dmat[i,t,x[i,t],])
+			psi[t,]=psi[t,]/sum(psi[t,])
 		}
 		states[i,T]=which.max(psi[T,])
 		for(t in (T-1):first[i])
 			states[i,t]=which.max(psi[t,]*gamma[i,t,,states[i,t+1]])
+		# check to make sure predicted state matches any observed state
+		observed=object$data$ehmat[i,]-1
+		if(any(states[i,]!=observed & observed!=0))
+		{
+		  warning("\nFor the following observation the predicted states conflict with the observed states")
+		  cat("\n",object$data$data[i,])
+		  cat("\npredicted")
+		  cat("\n",states[i,])
+		}
 	}
 	states=t(apply(states,1,function(x) state.names[x]))
 	return(states)
 }
 
-abundance_estimate=function(object,par=NULL)
+#' Estimate of abundance
+#' 
+#' Computes total abundance by occasion and abundance by stratum and occasion
+#' for fitted MSJS and MSJSU models
+#' 
+#' @param object fitted crm model (must be an MSJS or MSJSU model)
+#' @param par if not NULL use these values for matrix calculations
+#' @param state.names names to over-ride default names for states
+#' @author Jeff Laake
+#' @return dataframe of abundance estimates
+#' @export abundance_estimate
+#' @keywords utility
+abundance_estimate=function(object,par=NULL,state.names=NULL)
 {
+  # if par is NULL use the fitted parameter values otherwise it uses those
+  # specified in par which is used for bootstrapping
   if(!object$model%in%c("MSJS","MSJSU")) stop("Only used for JS type models")
+  # compute dmat and gamma matrices - then get predicted states
+  parmlist=compute_matrices(object,state.names=state.names,par=par)
+  object$results$mat=parmlist
   gd=global_decode(object,par=par)
+  # get frequencies of each history ignoring first one which is all 0's
   freq=object$data$freq[-1]
+  # replicate each set of predicted states by frequency
   gd=gd[rep(1:nrow(gd),freq),]
+  # get count of predicted states by strata over columns(occasions)
   strata=object$data$strata.labels
   strata=strata[strata!="N"]
   xx=apply(gd,2,function(x)table(factor(x,levels=strata)))
-  est=rep(object$results$f$report(par=par)$allN,object$data$nocc)*xx/sum(freq)
+  # compute abundance by multiplying total abundance by proportion in each state for each occasion
+  # this excludes any that are dead or still in state N
+  est=rep(object$results$mat$N,object$data$nocc)*xx/sum(freq)
+  # remove first occasion which is prior to sampling occasions (all in N state)
   est=est[,-1]
+  # get totals over strata and create dataframe
   est=rbind(est,colSums(est))
+  est=floor(est)
   rownames(est)[3]="Total"
-  return(floor(est))
+  N=data.frame(occasion=rep(1:(object$data$nocc-1),nrow(est)),strata=rep(rownames(est),each=10),N=as.vector(t(est)))
+  return(N)
 }
 
-# call compute_matrices and grab total abundance then call global decode
-
-abundance_estimate_bs=function(object,nreps=1000)
+#' Bootstrap limits for abundance estimation 
+#' 
+#' Computes bootstrap std error and confidence limits for estimated abundance in a MSJS or MSJSU model
+#' 
+#' @param object fitted crm model of type MSJS or MSJSU
+#' @param nreps number of bootstrap replicates
+#' @param alpha value for confidence interval 
+#' @author Jeff Laake
+#' @return dataframe of abundance estimates and std error and confidence intervals
+#' @export abundance_estimate_bs
+#' @keywords utility
+abundance_estimate_bs=function(object,nreps=1000,alpha=0.05)
 {
+  if(!object$model%in%c("MSJS","MSJSU")) stop("Only used for JS type models")
+  if(alpha>=1 | alpha <=0)stop("Invalid value of alpha")
+  # get abundance estimate dataframe with fitted parameter values
+  N=abundance_estimate(object)
+  #generate nreps vectors of parameters from multivariate-normal
   par_reps=rmvnorm(nreps,mean=unlist(object$results$beta),sigma=object$results$beta.vcv)
-  boot_reps=vector("list",length=nreps)
-  for(i in 1:nreps)
-    boot_reps[[i]]=abundance_estimate(object,par=par_reps[i,])
-  return(boot_reps)
+  # generate abundance estimate for first replicate and then create matrix with
+  # a column for each estimate
+  est=abundance_estimate(object,par=par_reps[1,])
+  boot_reps=matrix(NA,ncol=nrow(est),nrow=nreps)
+  colnames(boot_reps)=paste(est$occasion,est$strata,sep="-")
+  boot_reps[1,]=est$N
+  # loop over remaining replicates and store in matrix boot_reps
+  for(i in 2:nreps)
+    boot_reps[i,]=abundance_estimate(object,par=par_reps[i,])$N
+  # construct integer values for lower and upper limit of sorted replicate values
+  Llimit=floor(nreps*alpha/2)
+  Ulimit=floor(nreps*(1-alpha/2))
+  # get confidence limits for each estimate
+  CLimits=apply(boot_reps,2,function(x) {
+    xsort=sort(x)
+    c(xsort[Llimit],xsort[Ulimit])
+  })
+  # get std error for each estimate
+  se=apply(boot_reps,2,function(x) sqrt(var(x)))
+  # return estimates with se and limits
+  N=cbind(N,se,t(CLimits))
+  colnames(N)[4:6]=c("SE","LCL","UCL")
+  return(N)
 }
 
